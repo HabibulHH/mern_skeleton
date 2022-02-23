99a215faac7239926f27dbf0a97add6bc8939eec
HabibulHH <habibul.hasan.hira@gmail.com>
2022-02-23 22:40:07 +0600

    test changes

--- routers/login/loginRouter.js
+++ routers/login/loginRouter.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
+
 const { getLogin } = require("../../controller/loginController");
 const { auth, access } = require("../../middlewares/common/ErrorHandler");
+
 router.get("/", [auth, access], getLogin);
 module.exports = router;
+const express = require("express");
+const router = express.Router();
+
+const { getLogin } = require("../../controller/loginController");
+const { auth, access } = require("../../middlewares/common/ErrorHandler");
+
+// router.get("/", [auth, access], getLogin);
+// module.exports = router;
